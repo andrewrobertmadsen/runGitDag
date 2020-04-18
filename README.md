ac13c05250c982c8013be9eb289ef6758de63d34
andrew-madsen <andrewrobertmadsen@gmail.com>
2020-04-17 21:49:50 -0600

    And so it begins.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # runGitDag
-runGitDag
+
+runGitDag is a way to run users job code from version control on your environment. It enables analysts and data
+scientists to have more freedom and velocity by decoupling their work from data engineering and operations deploy cycle.
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hey there, Casey! I think we should try gin for our v1 of runGitDag. What do you think?")
+	})
+	router.Run(":3333")
+}
