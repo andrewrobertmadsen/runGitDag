8d988c33b80c382dc6d94f0ff349f7d4a7d4e04a
andrew-madsen <andrewrobertmadsen@gmail.com>
2020-04-20 22:12:42 -0600

    Working on some tests for job.

--- models/Job.go
+++ models/Job.go
@@ -1,6 +1,14 @@
 package models
 
+import "io"
+
 type Job struct {
 	RepoUrl   string
 	RepoPath   string
+}
+
+func (j Job) SaveJob(writer io.Writer) (err error) {
+	writer.Write("hello")
+
+	return
 }
--- models/Job_test.go
+++ models/Job_test.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestSaveJob(t *testing.T) {
+	test_job := Job{"this is a repo url", "path/in/repo"}
+	var buf bytes.Buffer
+	writer := io.Writer(&buf)
+	test_job.SaveJob(writer)
+	got := buf.String()
+	want := "Hello"
+
+	if got != want {
+		t.Errorf("Got %q, want %q", got, want)
+	}
+
+}
